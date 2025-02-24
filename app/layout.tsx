937ca18f28446f344c02e9b5cbc02d9a1ee44f07
dbolivar25 <danielbolivarflorez@gmail.com>
2025-02-24 13:41:07 -0600

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,26 @@ export const metadata: Metadata = {
     template: "%s | Daniel Bolivar",
   },
   description: "My personal site and portfolio.",
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png" }],
+    other: [
+      {
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
   openGraph: {
     title: "Daniel Bolivar",
     description: "My personal site and portfolio.",
