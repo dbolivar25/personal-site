4fd365fafa290d58a44211d1ec5b76e80e319c6d
dbolivar25 <danielbolivarflorez@gmail.com>
2025-05-13 21:50:54 -0400

    change intro

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage(): JSX.Element {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">Welcome</h1>
       <p className="mb-4">
         {`
-I'm Daniel Bolivar, software engineer at Augment AI and Computer Science student at Vanderbilt.
+I'm Daniel Bolivar, AI/ML Software Engineer at Augment AI and Computer Science Alumnus from Vanderbilt.
 I design and build high-performance AI/ML systems powering enterprise applications.
         `}
       </p>
